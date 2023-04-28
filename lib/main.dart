884bfb83405014821b3b0d22537de4572c3addb1
llucasmota <lucas.o.mota@gmail.com>
2023-04-28 08:23:19 -0300

    feat(main): adicionando uso das rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:rick_morty_app/components/character_component.dart';
+import 'package:rick_morty_app/screens/character_screen.dart';
+import 'package:rick_morty_app/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,63 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          actions: [],
-          backgroundColor: const Color.fromRGBO(151, 206, 76, 0.9),
-          elevation: 10,
-        ),
-        backgroundColor: Color.fromRGBO(255, 255, 255, 0.8),
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  alignment: Alignment.bottomCenter,
-                  image: AssetImage('assets/images/fallback_rick.png'),
-                  fit: BoxFit.contain,
-                  opacity: 0.2)),
-          child: Column(children: [
-            Container(
-              margin: const EdgeInsets.only(top: 20, left: 10),
-              alignment: Alignment.center,
-              child: const Text(
-                'Rick and Morty',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(12),
-              child: const Text(
-                'Rick and Morty Finder book check here the status of your favorite character',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(left: 10, right: 10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Color.fromRGBO(242, 242, 242, 0.6)),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(24),
-                          isDense: true,
-                          hintText: 'Search characters',
-                          border:
-                              OutlineInputBorder(borderSide: BorderSide.none)),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Column(
-              children: const [CharacterComponent()],
-            )
-          ]),
-        ),
-      ),
+      routes: {AppRoutes.HOME: (context) => const CharacterScreen()},
     );
   }
 }
